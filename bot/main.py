fb9034cc675dbd5a115f282bcf4bfb058365da5f
almogbaku <almog.baku@gmail.com>
2023-05-01 23:48:10 +0300

    fix: endless recovery

--- bot/main.py
+++ bot/main.py
@@ -21,17 +21,14 @@ def run():
 
     chat_mgr_grpc_url = os.environ.get('CHAT_MGR_GRPC_URL', 'unix:///tmp/chat-mgr.sock')
 
-    tries = 0
-    while tries < 5:
+    while True:
         try:
             with grpc.insecure_channel(chat_mgr_grpc_url) as channel:
                 future = executor.submit(start_bot, channel, db_uri)
                 future.result()
         except Exception as e:
             logger.exception(e)
-            logger.info(f"Retrying in 5 seconds... ({tries + 1}/5)")
-            tries += 1
-            sleep(5)
+            sleep(2)
 
 
 if __name__ == '__main__':
